54fa7689d0f0870d5637bf596e5433f118652d3f
705H120 <705h120@gmail.com>
2016-09-15 00:45:46 +0200

    + PUT/DELETE, + check if request is Ajax

--- synroute.php
+++ synroute.php
@@ -4,19 +4,38 @@ class SynRoute {
     private $_uri = '';
     private $_callback = '';
     private $_type = '';
-    private $url = '';
+    private $_basepath = '';
+    private $_xhr = '';
+
+    public function debug(){
+        //return [$this->_basepath, $this->_uri, $this->_type, $this->_xhr];
+        $arr = [];
+        foreach($this->_uri as $k => $v){
+            $arr[] = array('route' => $v, 'method' => $this->_type[$k], 'ajax' => $this->_xhr[$k]);
+        }
+        return $arr;
+    }
 
     public function __construct(){
         $basepath = implode('/', array_slice(explode('/', $_SERVER['SCRIPT_NAME']), 0, -1)) . '/';
-        $url = substr($_SERVER['REQUEST_URI'], strlen($basepath));
-        $this->url = '/' . trim($url, '/');
+        $_basepath = substr($_SERVER['REQUEST_URI'], strlen($basepath));
+        $this->_basepath = '/' . trim($_basepath, '/');
     }
 
     public function norewrite(){
-        $this->url = $_SERVER['QUERY_STRING'];
+        $this->_basepath = $_SERVER['QUERY_STRING'];
     }
 
     public function add($type, $uri, $callback){
+        $checkXHR = explode(':', $uri);
+        $xhr = 0;
+
+        if($checkXHR[0] == "x" OR $checkXHR[0] == "xhr") {
+            $uri = $checkXHR[1];
+            $xhr = 1;
+        }
+
+        $this->_xhr[] = $xhr;
         $this->_uri[] = $uri;
         $this->_callback[] = $callback;
         $this->_type[] = $type;
@@ -30,9 +49,35 @@ class SynRoute {
         $this->add('POST', $uri, $callback);
     }
 
+    public function put($uri, $callback){
+        $this->add('PUT', $uri, $callback);
+    }
+    public function delete($uri, $callback){
+        $this->add('DELETE', $uri, $callback);
+    }
+
+    public function checkXHR($k){
+        //return ($this->_xhr[$k] == 1 ? (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest' ? true : false) : true );
+        if($this->_xhr[$k] == 1){
+            if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+                return 1;
+            }
+            else {
+                return 0;
+            }
+        }
+        else {
+            return 1;
+        }
+    }
+
+    public function rMethod($k){
+            return ($_SERVER['REQUEST_METHOD'] == $this->_type[$k] ? true : false);
+    }
+
     public function run(){
         foreach($this->_uri as $k => $v){
-            if(preg_match("#^$v$#", $this->url, $params) && $_SERVER['REQUEST_METHOD'] == $this->_type[$k]){
+            if(preg_match("#^$v$#", $this->_basepath, $params) && $this->rMethod($k) && $this->checkXHR($k)){
                 array_shift($params);
                 call_user_func_array($this->_callback[$k], $params);
             }
